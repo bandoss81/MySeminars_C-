e03e58bcf35479833cea45f2baa6f9258ae860ef
bandoss81 <bandoss81@gmail.com>
2022-06-27 13:07:20 +0300

    Add Seminar 001

---
+++ Seminar001/Program.cs
@@ -0,0 +1,41 @@
+﻿/* 1 задача
+int num1, num2, quad;
+
+Console.Write("Input a first number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input a first number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+quad = num1 * num2;
+
+if(quad == num1)
+{
+    Console.WriteLine("First number is a quad of second number");
+}
+else
+{
+    Console.WriteLine("First number is not a quad of second number");
+}
+*/
+
+
+
+
+int n, current;
+
+Console.Write("Input a number: ");
+n = Convert.ToInt32(Console.ReadLine());
+
+current = n * (-1);
+
+while(current <= n)
+{
+    Console.Write(current + " ");
+    current++; // current = current +1
+}
+
+
+
+
+
