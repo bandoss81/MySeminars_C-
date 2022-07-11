0539ac06ba11ff09b9844cae9e78fe3d9fd08551
bandoss81 <bandoss81@gmail.com>
2022-07-11 22:42:28 +0300

    addition Seminar 006

--- Seminar006/Program.cs
+++ Seminar006/Program.cs
@@ -42,7 +42,7 @@ for(int i = 0; i < myArray.Length; i++)
 // 1 / 0 -> 0 1
 
 // 18 -> 10010
-
+/*
 string ChangeDigitSistem(int num)
 {
     string resultNumber = string.Empty;
@@ -56,30 +56,71 @@ string ChangeDigitSistem(int num)
 }
 
 Console.WriteLine(ChangeDigitSistem(18));
-
+*/
 //a < b + c
 
 //Напишите программу, которая принимает на вход три числа и проверяет, может
 // ли существовать треугольник с сторонами такой длины.
 
+/*
+//Способ1
+
 void Triangle (int a, int b, int c)
 {
-
-
+    if (a < b + c && b < a + c && c < a + b)
+       Console.WriteLine("Да");
+    else
+       Console.WriteLine("Нет");
 }
+Console.Write("Input num1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input num2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input num3: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
 
-bool Triangle(int a)
+Triangle (num1, num2, num3);
+
+//способ2
+
+bool Triangle(int a, int b, int c)
+{
+    return a < b + c && b < a + c && c < a + b
+}
+bool isTriangle = false;
+isTriangle = Triangle(1, 2, 3);
+
+Console.WriteLine(isTriangle);
+*/
 
 // 0 1 1 2 3 5 8 13 21 34 55 ...
 
 //
 // Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два
 // числа Фибоначчи: 0 и 1.
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
 
+    Console.WriteLine();    
+}
 int[] Fibonacci(int n)
 {
     int[] newArray = new int[n];
+    Console.WriteLine("Creating array here: ");
+    newArray[0] = 0;
+    newArray[1] = 1;
+    for (int i = 2; i < n; i++)
+    {
+        newArray[i] = newArray[i - 1] + newArray[i - 2];
+    }
+    return newArray;
 }
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+ShowArray(Fibonacci(num));
 
 
 
